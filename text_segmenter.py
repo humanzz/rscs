2ebc3baf66bc227d74d5f8c8b26ee8727404c22b
unknown <omar@.lt.is>
2011-11-21 17:07:11 +0100

    Pos filtered Bigram phrases : OMAR Unknown

--- text_segmenter.py
+++ text_segmenter.py
@@ -1,5 +1,6 @@
 import os
 import nltk
+import string
 
 class TextSegmenter:
   corpusRoot = os.getcwd() + "/corpus/"
@@ -9,19 +10,33 @@ class TextSegmenter:
     self.filepath = self.corpusRoot + filename
 
   def isAdjAdv(self, x):
-      tags = ['JJ', 'JJR', 'JJS', 'RB', 'RBR', 'RBS']
-      [word,posTag] = x
-      return posTag in tags 
+	tags = ['JJ', 'JJR', 'JJS', 'RB', 'RBR', 'RBS']
+	[word,posTag] = x
+	return posTag in tags 
       
   def process(self):
-    file = open(self.filepath,'r')
-    text = file.read()
-    # pos-tag the sentences
-    words = nltk.word_tokenize(text)
-    posList = nltk.pos_tag(words)
-    # filter to get only adjective and adverbs
-    filteredList = filter(self.isAdjAdv, posList)
-    return [x[0] for x in filteredList]
+	file = open(self.filepath,'r')
+	text = file.read()
+	# pos-tag the sentences
+	words = nltk.word_tokenize(text)
+	posList = nltk.pos_tag(words)
+	#words_noP = [w for w in words if w not in string.punctuation]
+	#tag1 = ['JJ', 'JJ', 'RB', 'RBR','RBS','JJ','NN','NNS','RB','RBR','RBS','RB','RBR','RBS','RB','RBR','RBS','RB','RBR','RBS']
+	#tag2 = ['NN','NNS','JJ','JJ','JJ','JJ','JJ','JJ','VB','VB','VB','VBD','VBD','VBD','VBN','VBN','VBN','VBG','VBG','VBG']
+	tagTuples = [('JJ','NN'),('JJ','NNS'),('RB','JJ'),('RBR','JJ'),('RBS','JJ'),('JJ','JJ'),('NN','JJ'),('NNS','JJ'),('RB','VB'),('RBR','VB'),('RBS','VB'),('RB','VBD'),('RBR','VBD'),('RBS','VBD'),('RB','VBN'),('RBR','VBN'),('RBS','VBN'),('RB','VBG'),('RBR','VBG'),('RBS','VBG')]
+	wordTuples = list()
+	for w in range(1,(len(posList)-1)):
+		[w1,t1] = posList[w]
+		[w2,t2] = posList[w+1]
+		tp = t1,t2
+		bg = w1,w2
+		if tp in tagTuples:	
+			wordTuples.append(bg)
+	
+	# filted to get only adjective and adverbs
+	#filteredList = filter(self.isAdjAdv, posList)
+	#return [x[0] for x in filteredList]
+	return wordTuples
         
-#ts = TextSegmenter('www.bookdwarf.com/100t1.txt')
-#print ts.process()
+ts = TextSegmenter('www.bookdwarf.com/100t1.txt')
+print ts.process()
