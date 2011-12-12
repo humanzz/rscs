33af07d4f57fcead900e3d525628d0e86f3b6fff
Ahmed Sobhi <humanzz@hotmail.com>
2011-12-12 16:33:49 +0100

    Refactoring the search… Unfinished business!

--- search.py
+++ search.py
@@ -19,6 +19,7 @@ BING_APPID = open('bingappid','r').read()
 
 class Search:
   def search(base, params):
+    #print base + urllib.urlencode(params)
     return simplejson.load(urllib.urlopen(base + urllib.urlencode(params)))
   search = staticmethod(search)
   
@@ -27,5 +28,24 @@ class Search:
   google = staticmethod(google)
   
   def bing(query):
-    return Search.search('http://api.bing.net/json.aspx?Version=2.2&Market=en-US&Web.Count=1&Sources=Web&JsonType=raw&', {'AppId': BING_APPID, 'Query': query})
+    return Search.search('http://api.bing.net/json.aspx?Version=2.2&Market=en-US&Web.Count=1&Sources=Web+News+RelatedSearch&JsonType=raw&', {'AppId': BING_APPID, 'Query': query})
   bing = staticmethod(bing)
+  
+  def hits(query, search_engine = "bing"):
+    #print query
+    res = getattr(Search, search_engine)(query)
+    #print res['SearchResponse'].keys()
+    #print res['SearchResponse']['News'].keys(), res['SearchResponse']['News']['Total'], res['SearchResponse']['News']['Results']
+    if search_engine == "bing":
+      res = res['SearchResponse']['Web']['Total']
+    elif search_engine == "google":
+      res = res['responseData']['cursor']['estimatedResultCount']
+    return res
+  hits = staticmethod(hits)
+  
+  def hits_near(phrase, near_to, search_engine = "bing"):
+    return Search.hits('"{0}" near:9 {1}'.format(phrase, near_to), search_engine)
+  hits_near = staticmethod(hits_near)
+  
+#print Search.hits("ahmed near:9 sobhi")
+#print Search.hits_near("audi r8", "excellent")
